7e407ec5fde4ffdb43a4bb183df0883a00c6b369
Sartaj Singh <singhsartaj94@gmail.com>
2014-12-27 21:25:35 +0530

    setup.py

--- fsplit/parse/opfsplit.py
+++ fsplit/parse/opfsplit.py
@@ -8,7 +8,7 @@
 # Licensed under the MIT license.
 ##
 
-import oswrapper as osw
+import os
 import argparse # parsing the options
 import parent_parser
 
@@ -23,7 +23,7 @@ def parse_args():
     parser.add_argument('target', type=str, help=help)
 
     help = "Output destination. Defaults to cwd."
-    parser.add_argument('--dest', '-d', type=str, help=help, default=osw.pDir())
+    parser.add_argument('--dest', '-d', type=str, help=help, default=os.getcwd())
 
     help = "Number of splits. Defaults to 2."
     parser.add_argument('--num', '-n', type=int, help=help, default=2)
--- setup.py
+++ setup.py
@@ -8,3 +8,31 @@
 # Licensed under the MIT license.
 ##
 
+from fsplit import __version__
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+setup(
+    name = 'fsplit',
+    version = __version__,
+    author = 'Sartaj Singh',
+    author_email = 'singhsartaj94@gmail.com',
+    description = ('Split files into small portable chunks'),
+    long_description = open('README.rst').read() + '\n\n' + open('CHANGELOG.rst').read(),
+    license = 'MIT',
+    keywords = 'files chunk split parts portable join tar',
+    url = 'http://github.com/leosartaj/autosign',
+    packages=find_packages(),
+    scripts=['bin/fsplit', 'bin/fjoin'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
+
